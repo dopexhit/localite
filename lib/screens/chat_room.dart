0ed4dab75a90ec4ec08e743085b2a814774261b4
neha507 <itsmeneha957@gmail.com>
2021-01-23 15:25:01 +0530

    chat room init

---
+++ lib/screens/chat_room.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChatRoom extends StatefulWidget {
+  final String roomId;
+
+  ChatRoom({this.roomId});
+
+  @override
+  _ChatRoomState createState() => _ChatRoomState();
+}
+
+class _ChatRoomState extends State<ChatRoom> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/screens/service_provider_detail.dart
+++ lib/screens/service_provider_detail.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:localite/models/custom_user.dart';
 import 'package:localite/models/service_provider_data.dart';
+import 'package:localite/screens/chat_room.dart';
+import 'package:provider/provider.dart';
+
 class SPDetail extends StatefulWidget {
-  ServiceProviderData currentSp;
+  final ServiceProviderData currentSp;
   SPDetail({this.currentSp});
   @override
   _SPDetailState createState() => _SPDetailState();
@@ -10,6 +14,22 @@ class SPDetail extends StatefulWidget {
 class _SPDetailState extends State<SPDetail> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final loggedUser = Provider.of<CustomUser>(context);
+
+    return Scaffold(
+      body: Center(
+          child: IconButton(
+        icon: Icon(Icons.message),
+        onPressed: () {
+          String roomId = loggedUser.uid + '-' + widget.currentSp.uid;
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ChatRoom(
+                        roomId: roomId,
+                      )));
+        },
+      )),
+    );
   }
 }
