c10442775f439b1b697212a2ab0df825ccdabebe
neha507 <itsmeneha957@gmail.com>
2021-01-25 21:27:32 +0530

    user accepted page init

--- lib/screens/user_pending_requests.dart
+++ lib/screens/user_pending_requests.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:localite/models/custom_user.dart';
 import 'package:localite/screens/request_detailed_user.dart';
+import 'package:localite/screens/user_accepted_request.dart';
 
 final _firestore = FirebaseFirestore.instance;
 
